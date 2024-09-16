e3711bfa24f57228cd0ba6e492775280f6510343
Christa890 <christajoshy890@gmail.com>
2024-09-17 00:47:55 +0530

    Updates

---
+++ assets/ContentItem-D3tuK2aT.js
@@ -0,0 +1,11 @@
+import{d as s,j as t}from"./index-Ua0TCTGL.js";const n=s.img`
+  width: 100%;
+  aspect-ratio: 2 / 3;
+  object-fit: cover;
+  border-radius: 5px;
+`,r=s.div`
+  text-align: center;
+  font-size: 14px;
+  padding: 5px 0;
+  color: white;
+`,c=({item:i})=>{const{name:e,"poster-image":o}=i;return t.jsxs("div",{children:[t.jsx(n,{src:`https://test.create.diagnal.com/images/${o}`,alt:e}),t.jsx(r,{children:e})]})};export{c as default};
